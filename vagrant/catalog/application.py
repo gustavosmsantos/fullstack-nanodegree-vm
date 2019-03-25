5769ad8a721a90265c686d0640951e20ecf6ffa9
Gustavo Santos <gustavosms@localhost.localdomain>
2019-03-24 21:51:12 -0300

    including comments and removing unused code

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -29,43 +29,39 @@ if google_client_id is None:
     raise Exception(
         "Environment G_CLIENT_ID should provide the Google client id.")
 
-
+# Find all categories, used in layout
 def find_categories():
     dbSession = DbSession()
     categories = dbSession.query(Category).all()
     return categories
 
-
+# Generates token to prevent CSRF attacks
 def newState():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
     return state
 
-
 app.jinja_env.globals.update(find_categories=find_categories)
 app.jinja_env.globals.update(newState=newState)
 app.jinja_env.globals.update(google_client_id=google_client_id)
 
-
+# Main route
 @app.route('/', methods=['GET'])
 def main():
     return render_template("home.html")
 
-
-@app.route('/login')
-def showLogin():
-    return render_template("login.html")
-
 # Authentication strongly inspired in lesson's code
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    # prevents CSRF validating server-side generated token
     if request.args.get('state') != login_session['state']:
         return json_response("Invalid state parameter.", 500)
 
     code = request.data
     print("Actual code is: %s" % code)
 
+    # Exchange google's code for the token
     try:
         oauth_flow = flow_from_clientsecrets(
             'client_secrets.json', scope='openid')
@@ -77,6 +73,7 @@ def gconnect():
             "Failed to upgrade the authorization code. %s" %
             str(error), 401)
 
+    # Retrieve the token info by the access token
     access_token = credentials.access_token
     url = (
         'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' %
@@ -100,6 +97,7 @@ def gconnect():
     if stored_access_token is not None and gplus_id == stored_gplus_id:
         return json_response('Current user is already connected.', 200)
 
+    # Setup session with retrieved data
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
@@ -111,7 +109,7 @@ def gconnect():
     login_session['name'] = data['name']
     return redirect(url_for('main'))
 
-
+# utilitary method that formats the output in json format
 def json_response(message, statusCode):
     response = make_response(jsonify(message=message), statusCode)
     if statusCode >= 400:
@@ -119,7 +117,7 @@ def json_response(message, statusCode):
     response.headers['Content-Type'] = 'application/json'
     return response
 
-
+# ivalidates the token in google server.
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
@@ -138,6 +136,7 @@ def gdisconnect():
     return redirect(url_for('main'))
 
 
+# creates a new item, available if user is logged in
 @app.route('/items/new', methods=['GET', 'POST'])
 def new_item():
     if login_session.get('name') is None:
@@ -169,7 +168,7 @@ def new_item():
             category=Category(
                 name=selected_category)))
 
-
+# utilitary function to validate request inputs
 def validate_and_extract_form_params(request, params):
     final_params = {}
     for param_name in params:
@@ -179,7 +178,7 @@ def validate_and_extract_form_params(request, params):
         final_params[param_name] = form_value
     return final_params
 
-
+# Controller that lists all items from category
 @app.route('/categories/<category_name>', methods=['GET'])
 def list_category(category_name):
     dbSession = DbSession()
@@ -195,7 +194,7 @@ def list_category(category_name):
         flash('Category not found')
         return redirect(url_for('main'))
 
-
+# Controller that allows to edit an item that the logged user owns
 @app.route('/catalog/<item_name>/edit', methods=['GET', 'POST'])
 def edit_item(item_name):
     try:
@@ -227,7 +226,7 @@ def edit_item(item_name):
         flash('Item not found')
         return redirect(url_for('main'))
 
-
+# Controller that allows to delete an item that the logged user owns
 @app.route('/catalog/<item_name>/delete', methods=['GET', 'POST'])
 def delete_item(item_name):
     try:
@@ -255,7 +254,7 @@ def delete_item(item_name):
         flash('Item not found')
         return redirect(url_for('main'))
 
-
+# Provides the list of all categories
 @app.route('/catalog.json', methods=['GET'])
 def json_endpoint():
     dbSession = DbSession()
